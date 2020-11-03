24de1719d7bd0a1433bced63742a58da9692b1a9
MickaelFP <72392674+MickaelFP@users.noreply.github.com>
2020-11-03 19:43:37 +0100

    Update pong.js

--- js/pong.js
+++ js/pong.js
@@ -1,11 +1,12 @@
 let largeur=$("#balle").width();
 let gauche=parseInt($("#balle").css("left"));
 let haut=parseInt($("#balle").css("top"));
-alert(gauche)
+alert(gauche) //Nous permet d'afficher les coordonnées de la balle quand on accède à la page
 
 
 setInterval(function(){
-    gauche++;
+    //On défini pour la balle des paramètres de mouvement
+    gauche=gauche+1; 
     haut=haut+0.5;
     $("#balle").css("left",gauche);
     $("#balle").css("top",haut);
