c883ef0babf44d876f10a68253ff23e95d6f3c68
MickaelFP <72392674+MickaelFP@users.noreply.github.com>
2020-11-13 05:10:23 +0100

    Update interval.js

--- js/interval.js
+++ js/interval.js
@@ -11,27 +11,7 @@ let raquetteDroite = new Raquette($("#droite"));
 raquetteDroite.changeDirection();
 
 setInterval(function(){
-    
-    //On défini pour la balle des paramètres de mouvement
-    balle.gauche=balle.gauche+balle.vitesseX; 
-    balle.haut=balle.haut+balle.vitesseY;
- 
-    if(balle.gauche>terrain.largeur){
-        balle.gauche=terrain.largeur;
-        balle.vitesseX=balle.vitesseX*-1;
-    }
-    if(balle.gauche<0){
-        balle.gauche=0;
-        balle.vitesseX=balle.vitesseX*-1;
-    }
-    if(balle.haut>terrain.hauteur){
-        balle.gauche=terrain.hauteur;
-        balle.vitesseY=balle.vitesseY*-1;
-    }
-    if(balle.haut<0){
-        balle.haut=0;
-        balle.vitesseY=balle.vitesseY*-1;
-    }
+
 //On créait une boucle pour les fonction précédentes
     balle.majHTML();
 
