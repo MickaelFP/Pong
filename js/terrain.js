80fdc4b535e9ebe2524abb7e8dbf063b31651219
MickaelFP <72392674+MickaelFP@users.noreply.github.com>
2020-11-13 03:02:42 +0100

    Create terrain.js

---
+++ js/terrain.js
@@ -0,0 +1,22 @@
+/* On créait une classe pour définir les paramètres du terrain */
+class Terrain{
+    constructor($element){
+        this.$element = $element;
+
+        this.largeur = $element.width();
+        this.hauteur = $element.height();
+    }
+
+    tilt(){
+        //ajouter une classe
+        this.$element.addClass("tilt");
+        let ici = this;
+
+        setTimeout(
+            function(){
+                //retirer une classe
+                ici.$element.removeClass("tilt");
+            },200
+        );
+    }
+}
