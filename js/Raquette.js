cfea8c365c6c6f8e4a5ac6523de0ea155e42ba71
MickaelFP <72392674+MickaelFP@users.noreply.github.com>
2020-11-19 21:45:37 +0100

    Update Raquette.js

--- js/Raquette.js
+++ js/Raquette.js
@@ -1,30 +1,68 @@
 // On créait une classe pour définir les paramètres de la raquette
 class Raquette{
     constructor($element){
+        
         this.$element = $element;
-
-        this.largeur = $element.width();
+        /**
+         *
+         * @type {number}
+         */
         this.hauteur = $element.height();
-
+        /**
+         *
+         * @type {number}
+         */
+        this.largeur = $element.width();
+        /**
+         *
+         * @type {number}
+         */
         this.positionX = parseInt($element.css("left"));
+        /**
+         *
+         * @type {number}
+         */
         this.positionY = parseInt($element.css("top"));
-
+        /**
+         *
+         * @type {number}
+         */
         this.direction = 0;
+        /**
+         *
+         * @type {number}
+         */
         this.vitesse = 3;
     }
 
+    /**
+     *
+     * @returns {number}
+     */
     get bas(){
         return this.positionY + this.hauteur;
     }
 
+    /**
+     *
+     * @param value
+     */
     set bas(value){
         this.positionY = value - this.hauteur;
     }
 
+    /**
+     *
+     * @returns {number}
+     */
     get droite(){
         return this.positionX + this.largeur;
     }
 
+    /**
+     *
+     * @param value
+     */
     set droite(value){
         this.positionX = value - this.largeur;
     }
