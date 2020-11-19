ce042f73b7ce2d8d8515fedf9751444012d0c0fc
MickaelFP <72392674+MickaelFP@users.noreply.github.com>
2020-11-19 21:03:31 +0100

    Update Interval.js

--- js/Interval.js
+++ js/Interval.js
@@ -1,8 +1,3 @@
-/* On défini les paramètres de la balle
-let largeur=$("#balle").width();
-let gauche=parseInt($("#balle").css("left"));
-let haut=parseInt($("#balle").css("top")); */
-
 // On créait une nouvelle fonction afin de definir les mouvents de la balle et de la raquette
 let terrain = new Terrain($("#terrain"));
 let balle = new Balle($("#balle"));
@@ -10,10 +5,8 @@ let raquetteGauche = new Raquette($("#gauche"));
 let raquetteDroite = new Raquette($("#droite"));
 raquetteDroite.changeDirection();
 
+// On créait une boucle pour les fonction précédentes
 setInterval(function(){
-
-/* On créait une boucle pour les fonction précédentes
-balle.majHTML(); */
 
     balle.bouger();
     
