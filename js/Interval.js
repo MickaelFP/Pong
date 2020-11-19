35f7f581d8c6ccd03f267a3d2c5eac2be539d36b
MickaelFP <72392674+MickaelFP@users.noreply.github.com>
2020-11-19 21:18:04 +0100

    Update Interval.js

--- js/Interval.js
+++ js/Interval.js
@@ -1,23 +1,7 @@
 // On créait une nouvelle fonction afin de definir les mouvents de la balle et de la raquette
-/**
- *Initialise un nouveau terrain pour le jeu
- * @type {Terrain}
- */
 let terrain = new Terrain($("#terrain"));
-/**
- *Initialise une nouvelle balle
- * @type {Balle}
- */
 let balle = new Balle($("#balle"));
-/**
- *Initialise une raquette gauche
- * @type {Raquette}
- */
 let raquetteGauche = new Raquette($("#gauche"));
-/**
- *Initialise une raquette droite
- * @type {Raquette}
- */
 let raquetteDroite = new Raquette($("#droite"));
 
 // On créait une boucle pour les fonction précédentes
