833cc27010ba7286d460f39c1de0161c7db7c08a
MickaelFP <72392674+MickaelFP@users.noreply.github.com>
2020-12-07 15:15:51 +0100

    Update Interval.js

--- js/Interval.js
+++ js/Interval.js
@@ -32,38 +32,38 @@ $("#btn-jouer").on("mouseup", function (event) {
 });
 
 //On fait appel aux addEventListener afin de pouvoir détecter et ainsi lire les touches utiliser
-window.addEventListener("keydown", function (event){
+window.addEventListener("keydown", function (event) {
     if (event.defaultPrevented){
         return
     }
 
     /* On va pouvoir définir les touches que l'on veut qu'ils détectent,
     ainsi que les conditions de déplacement des raquettes que l'on souhaite leurs associer */
-    if (event.key === "a"){
+    if (event.key === "a") {
         raquetteGauche.monter();
     }
-    if (event.key === "q"){
+    if (event.key === "q") {
         raquetteGauche.descendre();
     }
-    if (event.key === "p"){
+    if (event.key === "p") {
         raquetteDroite.monter();
     }
-    if (event.key === "m"){
+    if (event.key === "m") {
         raquetteDroite.descendre();
     }
     event.preventDefault();
 }, true);
 
-window.addEventListener("keyup", function (event){
+window.addEventListener("keyup", function (event) {
     if (event.defaultPrevented){
         return
     }
     /* On va pouvoir attribuer maintenant des conditions d'arrêt du mouvement des raquettes à nos touches,
     et ce afin que nos raquettes n'aient pas un mouvement continue après détection d'une touche */
-    if (event.key === "a" || event.key === "q"){
+    if (event.key === "a" || event.key === "q") {
         raquetteGauche.arret();
     }
-    if (event.key === "p" || event.key === "m"){
+    if (event.key === "p" || event.key === "m") {
         raquetteDroite.arret();
     }
     event.preventDefault();
